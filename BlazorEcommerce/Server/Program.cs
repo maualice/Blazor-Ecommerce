dc32aefeb340a0469d4b516479094555f87d7d96
Mauro Alice <malice@emotionit.com.ar>
2023-09-25 11:27:38 -0300

    Registering the DbContext & Configure SqlServer

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,11 +1,16 @@
 global using Microsoft.EntityFrameworkCore;
 global using BlazorEcommerce.Shared;
 using Microsoft.AspNetCore.ResponseCompression;
+using BlazorEcommerce.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -19,13 +24,13 @@ app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+	app.UseWebAssemblyDebugging();
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseSwagger();
