a3162ff4468fe37ab819016e7bd6deee2a05e080
Mauro Alice <malice@emotionit.com.ar>
2023-10-03 16:09:50 -0300

    using the ProductService in the ProductList Component

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,5 +1,6 @@
 global using System.Net.Http.Json;
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
